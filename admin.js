d34d0513bfac6db4e896e206ef02b2e8e8396fc8
apasionadoxquiniela <101425842+apasionadoxquiniela@users.noreply.github.com>
2026-05-05 16:54:11 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,18 +1,24 @@
 /* 
    GUIA PARA EL CLIENTE:
-   Para cambiar equipos, solo cambia el nombre y el numero de la 'id'.
-   Los escudos se cargan automaticamente usando el nombre del equipo.
+   Aquí gestionas las ligas y si los partidos son de Media Semana o Fin de Semana.
 */
 const DB_PARTIDOS = {
-    liga_mx: [
-        { id: "america", local: "América", visita: "Cruz Azul", imgL: "https://media.api-sports.io/football/teams/2287.png", imgV: "https://media.api-sports.io/football/teams/2289.png" },
-        { id: "chivas", local: "Guadalajara", visita: "Pumas UNAM", imgL: "https://media.api-sports.io/football/teams/2284.png", imgV: "https://media.api-sports.io/football/teams/2295.png" }
-    ],
-    premier: [
-        { id: "city", local: "Man. City", visita: "Liverpool", imgL: "https://media.api-sports.io/football/teams/50.png", imgV: "https://media.api-sports.io/football/teams/40.png" },
-        { id: "arsenal", local: "Arsenal", visita: "Man. United", imgL: "https://media.api-sports.io/football/teams/42.png", imgV: "https://media.api-sports.io/football/teams/33.png" }
-    ],
-    laliga: [
-        { id: "madrid", local: "Real Madrid", visita: "Barcelona", imgL: "https://media.api-sports.io/football/teams/541.png", imgV: "https://media.api-sports.io/football/teams/529.png" }
-    ]
+    liga_mx: {
+        media_semana: [
+            { id: "mx_m1", local: "América", visita: "Pachuca", imgL: "https://media.api-sports.io/football/teams/2287.png", imgV: "https://media.api-sports.io/football/teams/2283.png" },
+            { id: "mx_m2", local: "Tigres", visita: "Juárez", imgL: "https://media.api-sports.io/football/teams/2282.png", imgV: "https://media.api-sports.io/football/teams/2290.png" }
+        ],
+        fin_semana: [
+            { id: "mx_f1", local: "Chivas", visita: "Cruz Azul", imgL: "https://media.api-sports.io/football/teams/2284.png", imgV: "https://media.api-sports.io/football/teams/2289.png" },
+            { id: "mx_f2", local: "Pumas", visita: "Monterrey", imgL: "https://media.api-sports.io/football/teams/2295.png", imgV: "https://media.api-sports.io/football/teams/2281.png" }
+        ]
+    },
+    premier: {
+        media_semana: [
+            { id: "en_m1", local: "Chelsea", visita: "Everton", imgL: "https://media.api-sports.io/football/teams/49.png", imgV: "https://media.api-sports.io/football/teams/45.png" }
+        ],
+        fin_semana: [
+            { id: "en_f1", local: "Man. City", visita: "Arsenal", imgL: "https://media.api-sports.io/football/teams/50.png", imgV: "https://media.api-sports.io/football/teams/42.png" }
+        ]
+    }
 };
