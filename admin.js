500faf4a3a7387a9848b936b4e75a573eb21db27
apasionadoxquiniela <101425842+apasionadoxquiniela@users.noreply.github.com>
2026-05-05 13:48:07 -0600

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,6 @@
+// SOLO CAMBIA LO QUE ESTÁ ENTRE COMILLAS
+const JORNADA_CONFIG = [
+    { local: "América", visitante: "Cruz Azul" },
+    { local: "Pumas", visitante: "Chivas" },
+    { local: "Toluca", visitante: "Tigres" }
+];
