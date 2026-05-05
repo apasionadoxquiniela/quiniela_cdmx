ee51c0abec3708e7ab16200fe61a2ed1af3d765a
apasionadoxquiniela <101425842+apasionadoxquiniela@users.noreply.github.com>
2026-05-05 14:15:52 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,9 @@
-// SOLO CAMBIA LO QUE ESTÁ ENTRE COMILLAS
-const JORNADA_CONFIG = [
-    { local: "América", visitante: "Cruz Azul" },
-    { local: "Pumas", visitante: "Chivas" },
-    { local: "Toluca", visitante: "Tigres" }
-];
+const LIGAS_MUNDIALES = {
+    liga_mx: [
+        { id: 1, local: "América", visita: "Cruz Azul", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/9696_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/73d7_96x96.png", momio: 2.1 },
+        { id: 2, local: "Chivas", visita: "Pumas", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/4732_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/3234_96x96.png", momio: 1.8 }
+    ],
+    premier: [
+        { id: 3, local: "Man. City", visita: "Arsenal", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/89_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/42_96x96.png", momio: 2.5 }
+    ]
+};
