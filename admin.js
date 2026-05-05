cb6a0568513e2ebf5a433716fcdc6363835bd1a2
apasionadoxquiniela <101425842+apasionadoxquiniela@users.noreply.github.com>
2026-05-05 16:38:53 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,12 +1,18 @@
+/* 
+   GUIA PARA EL CLIENTE:
+   Para cambiar equipos, solo cambia el nombre y el numero de la 'id'.
+   Los escudos se cargan automaticamente usando el nombre del equipo.
+*/
 const DB_PARTIDOS = {
     liga_mx: [
-        { id: 1, local: "América", visita: "Cruz Azul", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/9696_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/73d7_96x96.png" },
-        { id: 2, local: "Monterrey", visita: "Tigres", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/7822_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/8082_96x96.png" }
+        { id: "america", local: "América", visita: "Cruz Azul", imgL: "https://media.api-sports.io/football/teams/2287.png", imgV: "https://media.api-sports.io/football/teams/2289.png" },
+        { id: "chivas", local: "Guadalajara", visita: "Pumas UNAM", imgL: "https://media.api-sports.io/football/teams/2284.png", imgV: "https://media.api-sports.io/football/teams/2295.png" }
     ],
     premier: [
-        { id: 3, local: "Arsenal", visita: "Man. United", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/42_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/6s_96x96.png" }
+        { id: "city", local: "Man. City", visita: "Liverpool", imgL: "https://media.api-sports.io/football/teams/50.png", imgV: "https://media.api-sports.io/football/teams/40.png" },
+        { id: "arsenal", local: "Arsenal", visita: "Man. United", imgL: "https://media.api-sports.io/football/teams/42.png", imgV: "https://media.api-sports.io/football/teams/33.png" }
     ],
     laliga: [
-        { id: 4, local: "Real Madrid", visita: "Barcelona", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/Th4f9_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/7_96x96.png" }
+        { id: "madrid", local: "Real Madrid", visita: "Barcelona", imgL: "https://media.api-sports.io/football/teams/541.png", imgV: "https://media.api-sports.io/football/teams/529.png" }
     ]
 };
