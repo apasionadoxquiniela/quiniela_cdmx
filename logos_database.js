aa807c3a1536d72f87c00ce70580ac95e3257438
apasionadoxquiniela <101425842+apasionadoxquiniela@users.noreply.github.com>
2026-05-05 17:31:55 -0600

    Create logos_database.js

---
+++ logos_database.js
@@ -0,0 +1,37 @@
+/* 
+   BASE DE DATOS UNIVERSAL DE LOGOS - QUINIELA CDMX
+   El sistema detecta el logo basándose en el nombre del equipo.
+*/
+const LOGOS_EQUIPOS = {
+    // LIGA MX
+    "America": "https://media.api-sports.io/football/teams/2287.png",
+    "Cruz Azul": "https://media.api-sports.io/football/teams/2289.png",
+    "Chivas": "https://media.api-sports.io/football/teams/2284.png",
+    "Pumas": "https://media.api-sports.io/football/teams/2295.png",
+    "Tigres": "https://media.api-sports.io/football/teams/2282.png",
+    "Monterrey": "https://media.api-sports.io/football/teams/2281.png",
+    "Pachuca": "https://media.api-sports.io/football/teams/2283.png",
+    "Toluca": "https://media.api-sports.io/football/teams/2280.png",
+    "Leon": "https://media.api-sports.io/football/teams/2286.png",
+    "Atlas": "https://media.api-sports.io/football/teams/2285.png",
+    "Mazatlan": "https://media.api-sports.io/football/teams/2279.png",
+    "Santos": "https://media.api-sports.io/football/teams/2288.png",
+    "Juarez": "https://media.api-sports.io/football/teams/2290.png",
+    "Tijuana": "https://media.api-sports.io/football/teams/2291.png",
+    "Necaxa": "https://media.api-sports.io/football/teams/2292.png",
+    "Atl. San Luis": "https://media.api-sports.io/football/teams/2293.png",
+    "Queretaro": "https://media.api-sports.io/football/teams/2294.png",
+    "Puebla": "https://media.api-sports.io/football/teams/2278.png",
+
+    // PREMIER LEAGUE
+    "Man. City": "https://media.api-sports.io/football/teams/50.png",
+    "Arsenal": "https://media.api-sports.io/football/teams/42.png",
+    "Liverpool": "https://media.api-sports.io/football/teams/40.png",
+    "Chelsea": "https://media.api-sports.io/football/teams/49.png",
+    "Man. United": "https://media.api-sports.io/football/teams/33.png",
+
+    // LA LIGA
+    "Real Madrid": "https://media.api-sports.io/football/teams/541.png",
+    "Barcelona": "https://media.api-sports.io/football/teams/529.png",
+    "Atletico Madrid": "https://media.api-sports.io/football/teams/530.png"
+};
