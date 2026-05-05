08eb9322c843ceaaa30e7688f7952373e232e20f
apasionadoxquiniela <101425842+apasionadoxquiniela@users.noreply.github.com>
2026-05-05 16:15:19 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,9 +1,13 @@
-const LIGAS_MUNDIALES = {
+const DB_PARTIDOS = {
     liga_mx: [
-        { id: 1, local: "América", visita: "Cruz Azul", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/9696_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/73d7_96x96.png", momio: 2.1 },
-        { id: 2, local: "Chivas", visita: "Pumas", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/4732_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/3234_96x96.png", momio: 1.8 }
+        { id: 1, local: "America", visita: "Cruz Azul", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/9696_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/73d7_96x96.png" },
+        { id: 2, local: "Chivas", visita: "Pumas", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/4732_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/3234_96x96.png" },
+        { id: 3, local: "Monterrey", visita: "Tigres", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/7822_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/8082_96x96.png" }
     ],
     premier: [
-        { id: 3, local: "Man. City", visita: "Arsenal", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/89_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/42_96x96.png", momio: 2.5 }
+        { id: 4, local: "Man. City", visita: "Liverpool", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/89_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/09_96x96.png" }
+    ],
+    laliga: [
+        { id: 5, local: "Real Madrid", visita: "Barcelona", imgL: "https://ssl.gstatic.com/onebox/media/sports/logos/Th4f9_96x96.png", imgV: "https://ssl.gstatic.com/onebox/media/sports/logos/7_96x96.png" }
     ]
 };
